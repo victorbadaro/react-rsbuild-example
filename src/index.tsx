a4599d835ff262576f301c4831c2e8bd9683b568
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-09-18 16:28:39 -0300

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom/client';
+import { App } from './app';
+
+import './index.css';
+
+const container = document.getElementById('root');
+
+ReactDOM.createRoot(container!).render(<App />);
